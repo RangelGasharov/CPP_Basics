9d2a4ba3f2db85576ae1572020cc95f33f3495d2
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-11-29 23:18:37 +0100

    Finished binary search

--- data types/BinarySearch.cpp
+++ data types/BinarySearch.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
-int numberArray[10] = {1, 1, 2, 4, 5, 7, 8, 9, 10, 10};
+int numberArray[] = {1, 2, 3, 4, 5, 5, 5, 5, 5, 5, 6, 6, 7,
+                     8, 8, 9, 10, 12, 15, 18, 19, 22, 28, 30};
 
-int binarySearch(int numberToFind) {
-    return numberToFind;
+
+int binarySearch(const int array[], int numberToFind) {
+    int leftIndex = 0;
+    int rightIndex = sizeof(numberArray) / sizeof(numberArray[0]) - 1;
+    int middleIndex;
+    while (leftIndex <= rightIndex) {
+        middleIndex = (rightIndex + leftIndex) / 2;
+        if (array[middleIndex] == numberToFind) {
+            return true;
+        } else if (numberToFind > array[middleIndex]) {
+            leftIndex = middleIndex + 1;
+        } else {
+            rightIndex = middleIndex - 1;
+        }
+    }
+    return -1;
 }
 
 int main() {
-    cout << binarySearch(2);
+    cout << binarySearch(numberArray, 19) << endl;
+    cout << binarySearch(numberArray, 11) << endl;
 }
