1d385069ff8cc92a7461d6fd105bffe3f6b9ede7
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-12-07 22:57:06 +0100

    Added an example of Object Functions

--- basics/Classes.cpp
+++ basics/Classes.cpp
@@ -20,15 +20,25 @@ public:
         author = aAuthor;
         pages = aPages;
     }
+
+    bool isOverThousandPages() {
+        if (pages >= 1000) {
+            return true;
+        }
+        return false;
+    }
 };
 
 int main() {
     Book book1("Lord of the Rings", "Tolkein", 720);
     Book book2("Harry Potter", "JK Rowling", 650);
     Book book3;
+    Book book4("The Count of Monte Cristo", "Alexandre Dumas", 1200);
 
     cout << book1.title << endl;
     cout << book2.title << endl;
     cout << book3.title << endl;
+    cout << book2.isOverThousandPages() << endl;
+    cout << book4.isOverThousandPages() << endl;
     return 0;
 }
