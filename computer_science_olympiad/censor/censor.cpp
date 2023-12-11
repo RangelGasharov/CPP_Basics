2bfae007fad65cb0d6324937977c4ed4b2218413
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-12-11 16:08:36 +0100

    Added slow algorithm

--- computer_science_olympiad/censor/censor.cpp
+++ computer_science_olympiad/censor/censor.cpp
@@ -2,11 +2,22 @@
 #include <iostream>
 
 std::string findBannedWord() {
-    std::string currentWord = "";
-    bool isFinished = false;
-    while (!isFinished) {
-        sendMessage("iamaninnocentpersonwhowouldneveruseanybadwords");
-    }
+    const int wordLength = 4;
+    const int messageLength = 10000;
+    const std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-    return "nope";
+    for (int i = 0; i < alphabet.length(); ++i) {
+        for (int j = 0; j < alphabet.length(); ++j) {
+            for (int k = 0; k < alphabet.length(); ++k) {
+                for (int l = 0; l < alphabet.length(); ++l) {
+                    std::string testWord = {alphabet[i], alphabet[j], alphabet[k], alphabet[l]};
+
+                    if (sendMessage(testWord)) {
+                        return testWord;
+                    }
+                }
+            }
+        }
+    }
+    return "";
 }
