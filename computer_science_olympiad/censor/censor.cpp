b98eb3ca44207111e7fe9da52025d5100e4cc208
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-12-12 22:18:44 +0100

    Altered algorithm

--- computer_science_olympiad/censor/censor.cpp
+++ computer_science_olympiad/censor/censor.cpp
@@ -1,23 +1,36 @@
 #include "censor.h"
 #include <iostream>
 
+using namespace std;
+
 std::string findBannedWord() {
     const int wordLength = 4;
     const int messageLength = 10000;
     const std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    
+    string text;
+    bool isFinished = false;
 
     for (int i = 0; i < alphabet.length(); ++i) {
         for (int j = 0; j < alphabet.length(); ++j) {
             for (int k = 0; k < alphabet.length(); ++k) {
                 for (int l = 0; l < alphabet.length(); ++l) {
-                    std::string testWord = {alphabet[i], alphabet[j], alphabet[k], alphabet[l]};
-
-                    if (sendMessage(testWord)) {
-                        return testWord;
-                    }
+                    text.append({alphabet[i], alphabet[j], alphabet[k], alphabet[l]});
                 }
             }
         }
     }
+
+    int currentIndex = 0;
+    string currentText = text.substr(currentIndex, messageLength);
+    while (!isFinished) {
+        if (sendMessage(currentText)) {
+            isFinished = true;
+        } else {
+            currentIndex += messageLength;
+            currentText = text.substr(currentIndex, messageLength);
+        }
+    }
+
     return "";
 }
