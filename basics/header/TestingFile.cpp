3798113c034cf65dd78b84775424c0ea997fbc22
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-01-27 23:29:31 +0100

    Added an example of header files in C++

---
+++ basics/header/TestingFile.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "TestingHeader.h"
+
+using namespace std;
+
+int main() {
+    std::cout << sum(1, 2) << std::endl;
+    return 0;
+}
---
+++ basics/header/TestingHeader.h
@@ -0,0 +1,9 @@
+#ifndef TestingHeader_h
+
+#define TestingHeader_h
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+#endif
