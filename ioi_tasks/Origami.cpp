ab8425d18c1b0cd959e45561d80636c8ae1a74e8
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-03-25 23:08:11 +0100

    Altered the function for filling up the table

--- ioi_tasks/Origami.cpp
+++ ioi_tasks/Origami.cpp
@@ -4,36 +4,29 @@
 
 using namespace std;
 
-int amountOfCuts{0};
-
-int findSmallestCombination(vector<vector<int>> table, int a, int b) {
-    /* int a = table[0].size();
-     int b = table.size();*/
+int findSmallestCombination(vector<vector<int>> &table, int longSide, int shortSide) {
     int smallestCombination{INT_MAX};
     int currentSum{INT_MAX};
 
-    if (a == b) {
-        return 0;
+    if (shortSide == longSide) {
+        return -1;
     }
-    if (a == 1) {
-        return b - 1;
-    }
-    if (b == 1) {
-        return a - 1;
+    if (shortSide == 1) {
+        return longSide - 2;
     }
 
-    for (int i = 0; i <= a / 2; i++) {
-        int x = a - 2 - i;
-        currentSum = table[b - 1][i] + table[b - 1][x];
+    for (int i = 0; i < shortSide / 2; i++) {
+        int x = shortSide - 2 - i;
+        currentSum = table[longSide - 1][i] + table[longSide - 1][x];
         if (currentSum < smallestCombination) {
             smallestCombination = currentSum;
         }
     }
 
-    for (int i = 0; i <= b / 2; i++) {
-        int y = b - 2 - i;
-        int n = table[i][a - 1];
-        int m = table[y][a - 1];
+    for (int i = 0; i < longSide / 2; i++) {
+        int y = longSide - 2 - i;
+        int n = table[i][shortSide - 1];
+        int m = table[y][shortSide - 1];
         currentSum = n + m;
         if (currentSum < smallestCombination) {
             smallestCombination = currentSum;
@@ -42,26 +35,16 @@ int findSmallestCombination(vector<vector<int>> table, int a, int b) {
     return smallestCombination;
 }
 
-void fillUpTable(vector<vector<int>> &table) {
-    int longSide, shortSide;
-    if (table.size() > table[0].size()) {
-        longSide = table.size();
-        shortSide = table[0].size();
-    } else {
-        shortSide = table.size();
-        longSide = table[0].size();
-    }
+void fillUpTable(vector<vector<int>> &table, int longSide, int shortSide) {
     for (int i = 0; i < longSide; i++) {
-        for (int j = 0; j < shortSide; j++) {
+        for (int j = 0; j < shortSide && j <= i; j++) {
             int value{0};
-            if (i == j) {
-                value = 0;
-            }
             value = findSmallestCombination(table, i + 1, j + 1) + 1;
             table[i][j] = value;
-            table[j][i] = value;
+            if (i < longSide) {
+                table[j][i] = value;
+            }
         }
-        std::cout << std::endl;
     }
 }
 
@@ -81,21 +64,24 @@ int getMinimumAmountOfCuts(int a, int b) {
     if (a == 1) { return b - 1; }
     if (b == 1) { return a - 1; }
 
-    vector<vector<int>> dp(b, vector<int>(a));
-    fillUpTable(dp);
-    printTable(dp);
+    int longSide, shortSide;
+    if (a > b) {
+        longSide = a;
+        shortSide = b;
+    } else {
+        longSide = b;
+        shortSide = a;
+    }
 
-    return dp[a - 1][b - 1];
+    vector<vector<int>> dp(longSide, vector<int>(shortSide));
+    fillUpTable(dp, longSide, shortSide);
+    return dp[longSide - 1][shortSide - 1];
 }
 
 int main() {
     int a{0};
     int b{0};
     cin >> a >> b;
-    //  cout << getMinimumAmountOfCuts(a, b) << endl;
-    cout << findSmallestCombination({{1, 2, 3},
-                                     {2, 0, 1},
-                                     {1, 3, 0},
-                                     {5, 4, 3}}, 3, 4);
+    cout << getMinimumAmountOfCuts(a, b) << endl;
     return 0;
 }
