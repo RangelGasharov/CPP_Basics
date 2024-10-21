b94eaf6232228621c684e51da690e05d16e15bdb
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-10-21 21:42:51 +0200

    Added a function that checks if the sum of the even digits in a number are greater than the sum of the odd digits

--- coding_contest/EvenOrOddGreater.cpp
+++ coding_contest/EvenOrOddGreater.cpp
@@ -1,3 +1,27 @@
-//
-// Created by gasha on 18.04.2024.
-//
+#include <string>
+#include <iostream>
+
+std::string evenOrOdd(std::string str) {
+    int sumEven{0}, sumOdd{0};
+    for (int i = 0; i < str.length(); i++) {
+        char currentNumberAsChar = str[i];
+        int currentNumber = currentNumberAsChar - '0';
+        if (currentNumber % 2 == 0) {
+            sumEven += currentNumber;
+        } else {
+            sumOdd += currentNumber;
+        }
+    }
+    if (sumEven > sumOdd) {
+        return "Even is greater than Odd";
+    } else if (sumOdd > sumEven) {
+        return "Odd is greater than Even";
+    } else {
+        return "Even and Odd are the same";
+    }
+}
+
+int main() {
+    std::cout << evenOrOdd("132");
+    return 0;
+}
